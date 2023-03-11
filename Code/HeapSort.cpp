ff8b548985c97277eae1f9fe8744c63a1634d37b
LeeKhanhs <127224032+LeeKhanhs@users.noreply.github.com>
2023-03-11 16:16:48 +0700

    Add files via upload

---
+++ Code/HeapSort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+const int n = 1000000;
+int a[1000005];
+void heapify(int N, int i) {
+    int Max = i;
+    int l = 2 * i + 1, r = 2 * i + 2;
+    if (l < N && a[l] > a[Max]) Max = l;
+    if (r < N && a[r] > a[Max]) Max = r;
+    if (Max != i) {
+        swap(a[i], a[Max]);
+        heapify(N, Max);
+    }
+}
+void heapSort() {
+    for (int i = n/2 - 1; i >= 0; --i) heapify(n, i);
+    for (int i = n-1; i >= 0; --i) {
+        swap(a[0], a[i]);
+        heapify(i, 0);
+    }
+}
+int main() {
+    for (int Test = 1; Test <= 10; ++Test) {
+        string filename = "input/test" + to_string(Test) + ".inp";
+        ifstream fi(filename);
+        for (int i = 0; i < n; ++i) fi >> a[i];
+        auto start = system_clock::now();
+        heapSort();
+        auto finish = system_clock::now();
+        cout << duration_cast<microseconds>(finish - start).count() <<'\n';
+    }
+    return 0;
+}
